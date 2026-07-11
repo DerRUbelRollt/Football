768015db24370796e88647d460c2bfb8327bfe9e
Max <maxrubel8@gmail.com>
2026-07-11 14:52:21 +0200

    Fix to connect with db

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var conn = builder.Configuration.GetConnectionString("Default") ?? "Host=localhost;Port=5433;Username=postgres;Password=TeamCompass2024;Database=team_compass_dev";
+var conn = builder.Configuration.GetConnectionString("Default") ?? "Host=127.0.0.1;Port=5433;Username=postgres;Password=TeamCompass2024;Database=team_compass_dev";
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(conn));
 
 var app = builder.Build();
